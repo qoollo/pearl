238ef0761916c004c062b27c49a064c9ff64b0a8
Pavel Yakushin <p.yakushin@qoollo.com>
2019-04-18 12:35:34 +0300

    create configurable generator

--- src/benchmark/bin.rs
+++ src/benchmark/bin.rs
@@ -5,10 +5,10 @@ mod generator;
 mod statistics;
 mod writer;
 
-use clap::App;
+use clap::{App, Arg, *};
 use futures::{executor::*, stream::*};
 
-use generator::Generator;
+use generator::{Config as GenConfig, Generator};
 use statistics::Statistics;
 use writer::Writer;
 
@@ -20,13 +20,42 @@ fn main() {
 }
 
 async fn start_app<S>(spawner: S) {
-    let matches = App::new("benchmark");
     println!("Hello Async World");
-    let mut generator = Generator::new();
+    println!("Prepare app matches");
+    let matches = prepare_matches();
+
+    println!("Create new generator");
+    let mut generator = Generator::new(
+        matches.value_of("value_size").unwrap().parse().unwrap(),
+        matches.value_of("key_size").unwrap().parse().unwrap(),
+        matches.value_of("memory").unwrap().parse().unwrap(),
+        matches.value_of("pregen").unwrap().parse().unwrap(),
+    );
+
+    println!("Create new writer");
     let writer = Writer::new();
+    println!("Create new statistics");
     let statistics = Statistics::new();
+    println!("Start write cycle");
     while let Some((key, value)) = await!(generator.next()) {
         let report = await!(writer.write());
         await!(statistics.add(report));
     }
+}
+
+fn prepare_matches<'a>() -> ArgMatches<'a> {
+    App::new("benchmark")
+        .arg(
+            Arg::with_name("value_size")
+                .short("v")
+                .default_value("90000"),
+        )
+        .arg(Arg::with_name("key_size").short("k").default_value("100"))
+        .arg(
+            Arg::with_name("memory")
+                .short("m")
+                .default_value("1000000000"),
+        )
+        .arg(Arg::with_name("pregen").short("p").default_value("0"))
+        .get_matches()
 }
--- src/benchmark/generator.rs
+++ src/benchmark/generator.rs
@@ -2,19 +2,40 @@ use futures::stream::Stream;
 use futures::task::*;
 use std::pin::*;
 
-pub struct Generator;
+pub struct Generator {
+    config: Config,
+}
 
 impl Generator {
-    pub fn new() -> Self {
-        Self
+    pub fn new(
+        avg_size_of_value: usize,
+        avg_size_of_key: usize,
+        memory_limit: usize,
+        num_obj_pregen: usize,
+    ) -> Self {
+        Self {
+            config: Config {
+                avg_size_of_value,
+                avg_size_of_key,
+                memory_limit,
+                num_obj_pregen,
+            },
+        }
     }
 }
 
 impl Stream for Generator {
-    type Item = ((), ());
+    type Item = (Vec<u8>, Vec<u8>);
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         println!("poll");
-        Poll::Ready(Some(((), ())))
+        Poll::Ready(Some((Vec::new(), Vec::new())))
     }
+}
+
+pub struct Config {
+    avg_size_of_value: usize,
+    avg_size_of_key: usize,
+    memory_limit: usize,
+    num_obj_pregen: usize,
 }
