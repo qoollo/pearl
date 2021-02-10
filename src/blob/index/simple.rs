579ec8dc8330a6ce7f94e26e1f46fc532dedc317
Ivan Druzhitskiy <idruzhitskiy@mail.ru>
2021-02-10 18:56:08 +0300

    add debug print for hashes

--- src/blob/index/simple.rs
+++ src/blob/index/simple.rs
@@ -257,6 +257,7 @@ impl Simple {
         header.written = 0;
         serialize_into(buf.as_mut_slice(), &header)?;
         let new_hash = get_hash(&buf);
+        error!("read hash: {:?}, new hash: {:?}", hash, new_hash);
         Ok(hash == new_hash)
     }
 
