816fa0e9fef47be973bef6d6855700d24418e79e
Perestoronin Pavel <p_perestoronin@mail.ru>
2021-05-26 17:59:41 +0300

    Add endian-based check

--- src/blob/index/range.rs
+++ src/blob/index/range.rs
@@ -22,16 +22,42 @@ impl RangeFilter {
             self.min = key_slice.to_vec();
             self.max = key_slice.to_vec();
             self.initialized = true;
-        } else if key_slice < &self.min {
+        } else if Self::lt(key_slice, &self.min) {
             self.min = key_slice.to_vec()
-        } else if key_slice > &self.max {
+        } else if Self::lt(&self.max, key_slice) {
             self.max = key_slice.to_vec()
+        }
+    }
+
+    pub(crate) fn lt(lv: &[u8], rv: &[u8]) -> bool {
+        if cfg!(target_endian = "big") {
+            lv < rv
+        } else {
+            // @FIXME: maybe there is a faster way to compare bytes of slices in reversed order?
+            if let CmpOrdering::Less = Iterator::cmp(lv.iter().rev(), rv.iter().rev()) {
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    pub(crate) fn le(lv: &[u8], rv: &[u8]) -> bool {
+        if cfg!(target_endian = "big") {
+            lv <= rv
+        } else {
+            // @FIXME: maybe there is a faster way to compare bytes of slices in reversed order?
+            if let CmpOrdering::Less = Iterator::cmp(rv.iter().rev(), lv.iter().rev()) {
+                false
+            } else {
+                true
+            }
         }
     }
 
     pub(crate) fn contains(&self, key: impl AsRef<[u8]>) -> bool {
         let key_slice = key.as_ref();
-        self.initialized && key_slice >= &self.min && key_slice <= &self.max
+        self.initialized && Self::le(&self.min, key_slice) && Self::le(key_slice, &self.max)
     }
 
     pub(crate) fn clear(&mut self) {
