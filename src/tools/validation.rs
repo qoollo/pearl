39a6d0bd94d2a363c78d9eeab025bd023aea2d1c
Vladimir Stepanenko <vovac12@gmail.com>
2022-05-16 16:37:34 +0300

    Fix index validation

--- src/tools/validation.rs
+++ src/tools/validation.rs
@@ -1,7 +1,7 @@
 use super::prelude::*;
 pub(crate) use crate::blob::header::Header as BlobHeader;
 use crate::blob::index::FileIndexTrait;
-pub(crate) use crate::blob::index::{header::IndexHeader, BPTreeFileIndex, SimpleFileIndex};
+pub(crate) use crate::blob::index::{header::IndexHeader, BPTreeFileIndex};
 use crate::blob::FileName;
 pub(crate) use crate::record::{Header, Record};
 pub use crate::BloomConfig;
@@ -12,19 +12,18 @@ fn block_on<T, F: Future<Output = T>>(f: F) -> Result<T> {
 }
 
 /// Validate index file
-pub fn validate_index<K: Key + 'static>(path: &Path) -> AnyResult<()> {
+pub fn validate_index<K>(path: &Path) -> AnyResult<()>
+where
+    for<'a> K: Key<'a> + 'static,
+{
+    static_assertions::const_assert_eq!(blob::index::HEADER_VERSION, 5);
     let header = read_index_header(path)?;
+    let blob_size = path.metadata()?.len();
     let headers = match header.version() {
-        2 => block_on(async {
-            let index =
-                <SimpleFileIndex as FileIndexTrait<K>>::from_file(FileName::from_path(path)?, None)
-                    .await?;
-            let res = index.get_records_headers().await?;
-            AnyResult::<_>::Ok(res.0)
-        })?,
-        3..=4 => block_on(async {
+        2..=4 => return Err(Error::index_header_validation_error("too low header version").into()),
+        5 => block_on(async {
             let index = BPTreeFileIndex::<K>::from_file(FileName::from_path(path)?, None).await?;
-            let res = index.get_records_headers().await?;
+            let res = index.get_records_headers(blob_size).await?;
             AnyResult::<_>::Ok(res.0)
         })?,
         _ => return Err(Error::index_header_validation_error("unknown header version").into()),
