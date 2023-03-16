8501c3b4993979f2f447e8d7a5dd7e8ec485b295
Ivan Druzhitskiy <idruzhitskiy@gmail.com>
2023-03-16 16:42:59 +0300

    use families instead of os

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,13 +1,6 @@
-#[cfg(target_os = "linux")]
+#[cfg(target_family = "unix")]
 mod unix;
-#[cfg(target_os = "linux")]
+#[cfg(target_family = "unix")]
 pub(crate) use unix::File;
-#[cfg(target_os = "linux")]
-pub use unix::IODriver;
-
-#[cfg(all(target_os = "macos", not(feature = "async-io-rio")))]
-mod linux;
-#[cfg(all(target_os = "macos", not(feature = "async-io-rio")))]
-pub(crate) use unix::File;
-#[cfg(all(target_os = "macos", not(feature = "async-io-rio")))]
+#[cfg(target_family = "unix")]
 pub use unix::IODriver;
