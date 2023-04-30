f76a835f5215e8b369e4bfd25fc363120dec614a
ikopylov <ikopylov@users.noreply.github.com>
2023-04-30 22:11:57 +0200

    [259] Add test clean-up where it is missed

--- tests/tests.rs
+++ tests/tests.rs
@@ -1114,13 +1114,14 @@ async fn test_in_memory_and_disk_records_retrieval() -> Result<()> {
         ));
     }
 
+    common::clean(storage, path).await.expect("clean success");
     Ok(())
 }
 
 #[tokio::test]
 async fn test_read_all_with_deletion_marker_delete_middle() -> Result<()> {
     let path = common::init("delete_middle");
-    let storage = common::default_test_storage_in(path).await.unwrap();
+    let storage = common::default_test_storage_in(&path).await.unwrap();
     let key: KeyTest = vec![0].into();
     let data: Bytes = "test data string".repeat(16).as_bytes().to_vec().into();
     storage.write(&key, data.clone()).await?;
@@ -1133,13 +1134,14 @@ async fn test_read_all_with_deletion_marker_delete_middle() -> Result<()> {
     assert!(!read[0].is_deleted());
     assert!(read[1].is_deleted());
 
+    common::clean(storage, path).await.expect("clean success");
     Ok(())
 }
 
 #[tokio::test]
 async fn test_read_all_with_deletion_marker_delete_middle_different_blobs() -> Result<()> {
     let path = common::init("delete_middle_blobs");
-    let storage = common::default_test_storage_in(path).await.unwrap();
+    let storage = common::default_test_storage_in(&path).await.unwrap();
     let key: KeyTest = vec![0].into();
     let data: Bytes = "test data string".repeat(16).as_bytes().to_vec().into();
     storage.write(&key, data.clone()).await?;
@@ -1155,5 +1157,6 @@ async fn test_read_all_with_deletion_marker_delete_middle_different_blobs() -> R
     assert!(!read[0].is_deleted());
     assert!(read[1].is_deleted());
 
+    common::clean(storage, path).await.expect("clean success");
     Ok(())
 }
