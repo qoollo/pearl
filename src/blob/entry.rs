e3cb259d0f2b8601dac2124b174e51be3bacc76d
ikopylov <ikopylov@users.noreply.github.com>
2023-06-24 23:14:17 +0200

    [231] Fix build errors after merge

--- src/blob/entry.rs
+++ src/blob/entry.rs
@@ -32,15 +32,15 @@ impl Entry {
             .read_exact_at_allocate(data_size + meta_size, self.header.meta_offset())
             .await
             .map_err(|err| err.into_bincode_if_unexpected_eof())
-            .with_context(|| format!("Record load failed from BLOB: {}", self.blob_file_name.to_path().display()))?;
+            .with_context(|| format!("Record load failed from BLOB: {}", self.blob_file_name.as_path().display()))?;
         let mut buf = buf.freeze();
         let data_buf = buf.split_off(meta_size);
         let meta = Meta::from_raw(&buf)
             .map_err(|err| Error::from(err))
-            .with_context(|| format!("Deserialization failed for Meta loaded from BLOB: {}", self.blob_file_name.to_path().display()))?;
+            .with_context(|| format!("Deserialization failed for Meta loaded from BLOB: {}", self.blob_file_name.as_path().display()))?;
         Record::new(self.header, meta, data_buf)
             .validate()
-            .with_context(|| format!("Validation failed for Record loaded from BLOB: {}", self.blob_file_name.to_path().display()))
+            .with_context(|| format!("Validation failed for Record loaded from BLOB: {}", self.blob_file_name.as_path().display()))
     }
 
     /// Returns only data.
@@ -52,7 +52,7 @@ impl Entry {
             .read_exact_at_allocate(self.header.data_size().try_into()?, data_offset)
             .await
             .map_err(|err| err.into_bincode_if_unexpected_eof())
-            .with_context(|| format!("Error loading Record data from BLOB: {}", self.blob_file_name.to_path().display()))?;
+            .with_context(|| format!("Error loading Record data from BLOB: {}", self.blob_file_name.as_path().display()))?;
         self.header.data_checksum_audit(&data)?;
         Ok(data)
     }
@@ -67,10 +67,10 @@ impl Entry {
             .read_exact_at_allocate(self.header.meta_size().try_into()?, meta_offset)
             .await
             .map_err(|err| err.into_bincode_if_unexpected_eof())
-            .with_context(|| format!("Failed to read Record metadata from BLOB: {}", self.blob_file_name.to_path().display()))?;
+            .with_context(|| format!("Failed to read Record metadata from BLOB: {}", self.blob_file_name.as_path().display()))?;
         let meta = Meta::from_raw(&buf)
             .map_err(|err| Error::from(err))
-            .with_context(|| format!("Deserialization failed for Meta loaded from BLOB: {}", self.blob_file_name.to_path().display()))?;
+            .with_context(|| format!("Deserialization failed for Meta loaded from BLOB: {}", self.blob_file_name.as_path().display()))?;
 
         self.meta = Some(meta);
         Ok(self.meta.as_ref())
