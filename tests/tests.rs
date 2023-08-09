d006f9654a2a936238174fd81c9541489fa8f349
Ivan Druzhitskiy <idruzhitskiy@gmail.com>
2023-08-09 17:10:59 +0300

    fix test

--- tests/tests.rs
+++ tests/tests.rs
@@ -446,38 +446,28 @@ async fn test_corrupted_blob_index_regeneration() {
 
     let blob_header_size = 2 * size_of::<u64>() + size_of::<u32>();
     let magic_len_size = size_of::<u64>() + size_of::<usize>();
-    let corruptions = [
-        // blob header corrupted
-        CorruptionType::ZeroedAtBegin(1),
-        // record header start corrupted
-        CorruptionType::ZeroedAt(blob_header_size as u64, 1),
-        // record header body corrupted
-        CorruptionType::ZeroedAt((blob_header_size + magic_len_size) as u64, 1)
-    ];
-    let len = corruptions.len();
+    let corruption = CorruptionType::ZeroedAt((blob_header_size + magic_len_size) as u64, 1);
 
-    for (i, corruption) in corruptions.into_iter().enumerate() {
-        let blob_file_path = path.join(format!("test.{i}.blob"));
-        let index_file_path = path.join(format!("test.{i}.index"));
-        assert!(blob_file_path.exists());
-        assert!(index_file_path.exists());
+    let blob_file_path = path.join("test.0.blob");
+    let index_file_path = path.join("test.0.index");
+    assert!(blob_file_path.exists());
+    assert!(index_file_path.exists());
 
-        common::corrupt_file(blob_file_path, corruption).expect("blob corruption failed");
-        std::fs::remove_file(index_file_path.clone()).expect("index removal");
-    }
+    common::corrupt_file(blob_file_path, corruption).expect("blob corruption failed");
+    std::fs::remove_file(index_file_path.clone()).expect("index removal");
     let new_storage = common::create_test_storage(&path, 1_000_000)
         .await
         .expect("storage should be loaded successfully");
     new_storage.close().await.unwrap();
 
-    for i in 0..len {
-        let index_file_path = path.join(format!("test.{i}.index"));
-        assert!(!index_file_path.exists());
-    }
+    let index_file_path = path.join("test.0.index");
+    assert!(!index_file_path.exists());
 
     let new_storage = common::create_test_storage(&path, 1_000_000)
         .await
         .expect("storage should be loaded successfully");
+    assert_eq!(new_storage.corrupted_blobs_count(), 1);
+
     common::clean(new_storage, path).await;
     warn!("elapsed: {:.3}", now.elapsed().as_secs_f64());
 }
